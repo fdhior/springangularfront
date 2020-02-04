84751c1129040bf827320fcc052cd6b886f6ccb6
VICTOR ALBERTO RAMIREZ CHAVEZ <VICG00M@telcel.net>
2020-02-04 17:37:58 -0600

    Se añadió el footer y el componente directiva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule
