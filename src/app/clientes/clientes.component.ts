e8e07f38e2ad1d807cc1e4b10121abd3d8a44bb9
Victor Alberto Ramirez Chavez <fdhior@gmail.com>
2020-05-04 02:04:00 -0500

    Cambios en los servicios que consumen servicios del backend

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -18,6 +20,42 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe (
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    });
+
+    swalWithBootstrapButtons.fire({
+      title: '¿Está seguro?',
+      text: `¿Seguro que desea eliminar el cliente ${cliente.nombre} ${cliente.apellido}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, ¡eliminar!',
+      cancelButtonText: 'No, ¡cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swalWithBootstrapButtons.fire(
+              '¡Cliente Eliminado!',
+              `Cliente ${cliente.nombre} eliminado con éxito`,
+              'success'
+            );
+          }
+        )
+
+      }
+    });
+
   }
 
 }
